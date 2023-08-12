1e823cb3b321a8ffc13a48279cd61b848fba261b
TSOlami <tijani.saheed@yahoo.com>
2023-08-12 22:03:30 +0100

    add async handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,11 @@
+import asyncHandler from express-asnyc-handler;
+
 // @desc	Authenticate user/set token
 // Route	post  /api/user/auth
 // access	Public
-const authUser = async (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: 'Auth User' })
-}
+});
 
 export {
 	authUser
