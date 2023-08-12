796c3a72f9d11c49d6d7236cf5f4a478c2c339f5
TSOlami <tijani.saheed@yahoo.com>
2023-08-12 21:36:18 +0100

    connect the user routes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,11 @@ dotenv.config();
 
 const port = process.env.PORT || 5000
 
+import userRoutes from './routes/userRoutes.js';
+
 const app = express();
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready'));
 
