cb561ab1940698e4b6d0bff463ba7a5b38ce89bb
TSOlami <tijani.saheed@yahoo.com>
2023-09-23 14:31:45 +0100

    version api

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+import { notFound, errorHandler } from './middleware/errormiddleware.js';
+
+const port = process.env.PORT || 5000
+
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
@@ -13,4 +17,7 @@ app.use(`/api/${apiVersion}/users`, userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready'));
 
-app.listen(port, () => {console.log(`Server started on port ${port}`)} );
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server is started on port ${port}`));
