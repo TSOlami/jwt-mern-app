28d26011166845e0e2615ecdaa2925048fd971a4
TSOlami <tijani.saheed@yahoo.com>
2023-09-29 21:33:23 +0100

    some more settings

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -26,7 +26,10 @@ export default {
         "coral-red": "#FF6452",
         "slate-gray": "#6D6D6D",
         "pale-blue": "#F5F6FF",
-        "white-400": "rgba(255, 255, 255, 0.80)"
+        "white-400": "rgba(255, 255, 255, 0.80)",
+        "black": "#000000",
+        "white": "#FFFFFF",
+        "blue": "#0074E4"
       },
       boxShadow: {
         '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
