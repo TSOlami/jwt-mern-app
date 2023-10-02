6c0a7edc30c56bc3d7d56defe8927a61a9fed5e8
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-02 23:48:30 +0100

    Landing Page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,47 +1,7 @@
-import * as Yup from "yup"
-import { useFormik } from "formik"
 
 const Landing = () => {
-  const initialvalues = {
-    fulllName: "",
-    username: "",
-    matricNumber: "",
-    password: "",
-  }
-  const validationSchema = Yup.object({
-    fulllName: Yup.string(),
-    username: Yup.string(),
-    matricNumber: Yup.string(),
-    password: Yup.string(),
-  })
-
-  const formik = useFormik({
-    initialValues: initialvalues,
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      console.log(values)
-    },
-  })
-
   return (
-    <div>
-      <div>
-        <h1>Welcome to NAMSSN Website</h1>
-        <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="fullname">Full Name</label>
-          <input
-            type="text"
-            name="fullname"
-            id="fullname"
-            onChange={formik.handleChange}
-            value={formik.values.fullname}  />
-        </form>
-      </div>
-
-      {/* Vector image container */}
-      <div></div>
-
-    </div>
+    <div>Landing</div>
   )
 }
 
