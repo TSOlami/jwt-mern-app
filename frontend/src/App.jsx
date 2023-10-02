102abf7b4977f5994a68f101872a3ccd60ce8c6b
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-02 07:19:55 +0100

    Signin page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/signin' element={<SignIn/>} />
+    </Routes>
+    </BrowserRouter>    
   )
 }
