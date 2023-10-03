b11b96bf86f53220714b2ae7e9d16c0ad359462c
TSOlami <tijani.saheed@yahoo.com>
2023-10-03 16:45:30 +0100

    add a few components

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,7 +1,27 @@
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        Hero
+      </section>
+      <section className="padding">
+        About Us
+      </section>
+      <section className="padding">
+        Features
+      </section>
+      <section className="padding">
+        FAQs
+      </section>
+      <section className="padding">
+        Contact Us
+      </section>
+      <section className="padding">
+        Footer
+      </section>
+    </main>
   )
 }
 
