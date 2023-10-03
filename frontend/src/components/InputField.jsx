3663f2e13b8c58bb023fad599b96e57457cbf6be
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-03 14:42:47 +0100

    Input field component

---
+++ frontend/src/components/InputField.jsx
@@ -0,0 +1,19 @@
+
+const InputField = ({ type, name, id, placeholder, value, onChange, icon }) => {
+  return (
+    <div className="flex flex-row relative w-full">
+      <input
+        type={type}
+        name={name}
+        id={id}
+        onChange={onChange}
+        value={value}
+        className="border-2 rounded border-gray-400 h-[40px] p-2 w-full pl-10"
+        placeholder={placeholder}
+      />
+      {icon && <div className="absolute left-2 flex self-center justify-center">{icon}</div>}
+    </div>
+  );
+};
+
+export default InputField;
