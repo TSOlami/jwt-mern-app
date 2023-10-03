838faddd3b3e3ec556042c2654dd75306d14d04c
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-03 23:45:08 +0100

    Improved mobile page styling

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -48,18 +48,18 @@ const SignUp = () => {
 	});
 
 	return (
-		<div className="flex flex-row md:bg-primary h-screen">
+		<div className="flex md:flex-row flex-col bg-primary h-screen">
 			{/* Vector image container */}
-			<div className="bg-primary h-screen hidden md:block md:w-[40%] relative">
+			<div className="bg-primary h-[20%] md:h-screen md:block w-full md:w-[40%]">
 				<img
 					src={Signup}
 					alt=""
-					className="absolute right-[-15%] top-[10%]"
+					className="relative h-[140%] m-auto md:h-auto md:right-[-15%] md:top-[10%]"
 				/>
 			</div>
 
-			<div className="w-full md:w-[60%] rounded-l-[10%] p-10 md:p-16 md:pl-36 bg-white flex flex-col justify-center">
-				<h1 className="font-bold text-2xl text-center p-10">
+			<div className="w-full h-full md:w-[60%] md:rounded-l-[10%] md:rounded-r-none rounded-t-[10%] p-10 md:p-16 md:pl-36 bg-white flex flex-col justify-center">
+				<h1 className="font-bold text-2xl text-center pt-5 md:p-10">
 					Welcome To NAMSSN Website
 				</h1>
 				<form onSubmit={formik.handleSubmit} className="flex flex-col">
@@ -75,6 +75,7 @@ const SignUp = () => {
 						value={formik.values.fullName}
 						icon={<FaRegUser />}
             pad
+            placeholder="Enter full name"
 					/>
 
 					{formik.touched.fullName && formik.errors.fullName ? (
@@ -93,6 +94,7 @@ const SignUp = () => {
 						value={formik.values.username}
 						icon={<FaIdCard />}
             pad
+            placeholder="Enter Username"
 					/>
 
 					{formik.touched.username && formik.errors.username ? (
@@ -111,6 +113,7 @@ const SignUp = () => {
 						value={formik.values.email}
 						icon={<FaEnvelope />}
             pad
+            placeholder='Enter email'
 					/>
 
 					{formik.touched.email && formik.errors.email ? (
@@ -129,6 +132,7 @@ const SignUp = () => {
 							value={formik.values.password}
 							icon={<FaLock />}
               pad
+              placeholder='Enter password'
 						/>
 						{showPassword ? (
 							<FaRegEyeSlash
