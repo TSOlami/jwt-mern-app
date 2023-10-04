1a1b12e9bd188e9f50796737167ac6a8cb33e7d4
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-04 05:27:10 +0100

    Add home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import Signup from './pages/Signup';
 import Landing from './pages/Landing';
+import Home from './pages/Home';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Route path='/' exact element={<Landing/>} />
       <Route path='/signin' element={<SignIn/>} />
       <Route path='/signup' element={<Signup/>} />
+      <Route path='/home' element={<Home/>} />
     </Routes>
     </BrowserRouter>    
   )
