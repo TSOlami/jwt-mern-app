4928855c55e30ec60cdcc6ffbf546e008802eca4
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-05 21:28:02 +0100

    Updtate colors

--- frontend/src/components/Announcement.jsx
+++ frontend/src/components/Announcement.jsx
@@ -7,7 +7,7 @@ const Announcement = ({title, content}) => {
       <div>
         {content?.map((text, index) => {
           return <div key={index} className="py-4">
-            <div className="flex flex-row items-center gap-2 font-semibold py-1">{text.user} {text.isAdmin && <FaCircleCheck/>}</div>
+            <div className="flex flex-row items-center gap-2 font-semibold py-1">{text.user} {text.isAdmin && <FaCircleCheck color="#17A1FA"/>}</div>
             <div>{text.content}</div>
           </div>
         })}
