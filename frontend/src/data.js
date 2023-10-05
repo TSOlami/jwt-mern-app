8a0f734fc25a229cc41494eb15cf6a1995e1c1f9
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-05 03:57:29 +0100

    Mock data

---
+++ frontend/src/data.js
@@ -0,0 +1,31 @@
+export const mockTexts = [
+	"Here's how the meetings go",
+	"Me: Okay, I understand what you ask, but it's actually impossible because...",
+	"Zeeil: I picked you to lead the design team because I know how good you are, you'll make it work",
+	"Me: But, but",
+];
+
+export const mockTexts2 = [
+  "Schools will now pay 2million naira for tax to the federal government",
+  "A townhall different from Balabluee",
+  "We will make Nigeria better by crook"
+]
+
+export const mockAnnouncements = [
+	{
+		user: "Ifedimeji Omonyi",
+		isAdmin: true,
+		content:
+			"There would be a departmental meeting to discuss the increased cost of things required to maintain the departmen",
+	},
+	{
+		user: "Slimanny",
+		isAdmin: false,
+		content: "lorem ipsum dolor and testing whatever this is",
+	},
+  {
+    user: "Elon musk",
+    isAdmin: true,
+    content: "I will be buying half of all Nigeria soon to help alleviate poverty. I will also employ 2000 techies into my space X company because I believe Nigeria is the future"
+  }
+];
