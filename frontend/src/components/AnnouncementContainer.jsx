0add2191a92a211e6853818abaa3ee99ce11e55f
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-05 21:28:43 +0100

    Incompletely responsive

--- frontend/src/components/AnnouncementContainer.jsx
+++ frontend/src/components/AnnouncementContainer.jsx
@@ -3,10 +3,16 @@ import Announcement from "./Announcement";
 
 const AnnouncementContainer = () => {
 	return (
-		<div className="border-gray-900 border-l-2 p-4">
-      <h1 className="text-3xl font-bold py-2 border-b-2 ">Announcements</h1>
+		<div className="border-gray-300 border-l-2 p-4 md:flex flex-col gap-3 hidden">
+			<h1 className="text-3xl font-bold py-2 border-b-2 ">
+				Announcements
+			</h1>
 			<Announcement
 				title="General Announcements"
+				content={mockAnnouncements}
+			/>
+			<Announcement
+				title="100l Announcements"
 				content={mockAnnouncements}
 			/>
 		</div>
