d26d1a93166175cb9dda9a84d17ff282db0c81a5
TSOlami <tijani.saheed@yahoo.com>
2023-10-06 11:07:57 +0100

    add about section

--- frontend/src/sections/AboutUs.jsx
+++ frontend/src/sections/AboutUs.jsx
@@ -1,6 +1,27 @@
+import aboutIcon from '../assets/icons/about.svg';
+import arrow from '../assets/icons/arrow.svg';
+
 const AboutUs = () => {
   return (
-    <div>About Us</div>
+    <section id="about-us" className="w-full flex lg:flex-row flex-col justify-center gap-8 max-container">
+      <div className='relative flex justify-center w-full padding-x gap-8'>
+        <img
+          src={aboutIcon}
+          alt="About Us"
+          width={90}
+          height={90}
+        /> <span className='header-text'>About Us</span>
+      </div>
+      <div className='flex-col'>
+        <p className="body-text">
+          At NAMSSN, we foster a community of passionate individuals who share a common love for mathematics and its applications. Our association brings together students, faculty, and enthusiasts to explore, learn, and grow together. Mathematics Department is hard like mad and i dont see wy a person would want a website designed for it. But i would try to make it beautiful sha. Using Illustratons and images
+        </p>
+        <div className="text-container flex border-primary">
+        Know more about us<img src={arrow} alt="arrow" />
+        </div>
+      </div>
+      
+    </section>
   )
 }
 
--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   className="w-full flex lg:flex-row flex-col justify-center min-h-screen gap-2 max-container"
   >
     <div className="relative lg:w-3/5 flex flex-col justify-center items-start w-full padding-x pt-28">
-    <div className="text-container">
+    <div className="text-container border-black">
     Unlock the World of Mathematics <span><img src={polygon} alt="polygon" /></span>
     </div>
     <div className="header-text">
