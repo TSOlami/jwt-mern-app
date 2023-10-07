341cf2c5438c367b541dc11d32e1bf1f83498360
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-08 00:08:49 +0100

    SVG images

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -9,5 +9,7 @@ import searchIcon from './searchIcon.png';
 import Avatar from './images/avatar2.png';
 import aboutIcon from './icons/about.svg';
 import arrow from './icons/arrow.svg';
+import FeesSVG from './images/FeesSVG.svg'
+import Bro from './images/bro.svg';
 
-export { NamssnLogo, SignInImage, SignUpImage, Wrapper, Hamburger, polygon, heroImage, searchIcon, Avatar, aboutIcon, arrow };
+export { NamssnLogo, SignInImage, SignUpImage, Wrapper, Hamburger, polygon, heroImage, searchIcon, Avatar, aboutIcon, arrow, FeesSVG, Bro };
