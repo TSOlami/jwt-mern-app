be261a7c2988e4ac5594f79298fe055cb8658d81
TSOlami <tijani.saheed@yahoo.com>
2023-10-07 16:23:09 +0100

    Add FAQ Section

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -9,5 +9,7 @@ import searchIcon from './searchIcon.png';
 import Avatar from './avatar2.png';
 import aboutIcon from './icons/about.svg';
 import arrow from './icons/arrow.svg';
+import FAQ from './images/FAQ.svg';
+import arrowDown from './icons/arrowDown.svg';
 
-export { NamssnLogo, SignInImage, SignUpImage, Wrapper, Hamburger, polygon, heroImage, searchIcon, Avatar, aboutIcon, arrow };
+export { NamssnLogo, SignInImage, SignUpImage, Wrapper, Hamburger, polygon, heroImage, searchIcon, Avatar, aboutIcon, arrow, FAQ, arrowDown };
--- frontend/src/sections/FAQs.jsx
+++ frontend/src/sections/FAQs.jsx
@@ -1,45 +1,78 @@
-import image4 from '../assets/images/image4.svg'
+import { useState } from 'react';
+import { FaSortUp, FaSortDown } from 'react-icons/fa';
+import { FAQ } from '../assets';
+
+const FAQItem = ({ questionNumber, question, answer }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-row justify-between items-center border-b-2">
+        <h2 className="text-lg font-normal font-merriweather">
+          <span className="text-3xl font-bold font-merriweather">{questionNumber}. </span>
+          {question}
+        </h2>
+        <button onClick={toggleExpanded}>
+          {expanded ? <FaSortUp /> : <FaSortDown />}
+        </button>
+      </div>
+      {expanded && (
+        <div className=" p-2 flex flex-row">
+          <p className="body-text">{answer}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 const FAQs = () => {
   return (
-	<section>
-    <h1 className="header-text text-center max-w-xl mx-auto">Why You Should Use NAMSSN Website</h1>
-    <div className="flex flex-col pt-16 sm:pt-10">
-      <div className="flex flex-col lg:flex-row gap-10">
-        <div className="relative flex-row-reverse bg-tertiary rounded-2xl shadow-xl">
-         <div className="flex flex-col items-start pl-10 py-20">
-         <h3 className="text-3xl font-bold font-crimson">Blog</h3>
-         <p className="text-xl font-crimson">Get access to daily educational blog posts to keep you informed about the daily happenings in the department. Get your questions answered without issues and never be clueless about the department</p>
-         <img src={image4} alt="" />
-         </div>
-         
-        </div>
-        <div className="relative flex-row bg-tertiary rounded-2xl max-w-[25rem] shadow-xl">
-         <div className="flex flex-col items-start pl-10 py-20">
-         <h3 className="text-3xl font-bold font-crimson">Blog</h3>
-         <p className="text-xl font-crimson">Get access to daily educational blog posts to keep you informed about the daily happenings in the department. Get your questions answered without issues and never be clueless about the department</p>
-         </div>
-        </div>
-      </div>
-      
-      <div className="flex flex-col lg:flex-row gap-10 pt-8">
-        <div className="relative flex-row bg-tertiary rounded-2xl max-w-[25rem] shadow-xl">
-         <div className="flex flex-col items-start pl-10 py-20">
-         <h3 className="text-3xl font-bold font-crimson">Blog</h3>
-         <p className="text-xl font-crimson">Get access to daily educational blog posts to keep you informed about the daily happenings in the department. Get your questions answered without issues and never be clueless about the department</p>
-         </div>
-        </div>
-        <div className="relative flex-row-reverse bg-tertiary rounded-2xl shadow-xl">
-         <div className="flex flex-col items-start pl-10 py-20">
-         <h3 className="text-3xl font-bold font-crimson">Blog</h3>
-         <p className="text-xl font-crimson">Get access to daily educational blog posts to keep you informed about the daily happenings in the department. Get your questions answered without issues and never be clueless about the department</p>
-         </div>
-         <img src={image4} alt="" />
-        </div>
-      </div>
-    </div>
-  </section>
-  )
-}
+    <section className="w-full mx-auto">
+      <h1 className="header-text text-center max-w-xl mx-auto">
+        Frequently Asked Questions
+      </h1>
+      <div className="flex md:flex-row-reverse flex-col gap-10 pt-10 pr-8 mx-auto">
+        <div className="flex flex-col">
+        <FAQItem
+      questionNumber={1}
+  question="What does NAMSSN stand for?"
+  answer="NAMSSN stands for the National Association of Mathematical Sciences Students of Nigeria."
+/>
 
-export default FAQs
+<FAQItem
+  questionNumber={2}
+  question="Is this website affiliated with NAMSSN at the national level?"
+  answer="No, this website is specifically for the NAMSSN FUTMINNA chapter, which is based at the Federal University of Technology Minna."
+/>
+
+<FAQItem
+  questionNumber={3}
+  question="How can I become a member of NAMSSN FUTMINNA?"
+  answer="To become a member of NAMSSN FUTMINNA, you can visit our Membership page and follow the registration process outlined there."
+/>
+
+<FAQItem
+  questionNumber={4}
+  question="What benefits do NAMSSN members receive?"
+  answer="NAMSSN members enjoy various benefits, including access to educational resources, participation in events, and networking opportunities with fellow mathematics enthusiasts."
+/>
+
+<FAQItem
+  questionNumber={5}
+  question="Can I contribute to the content on this website?"
+  answer="Yes, we welcome contributions from our members. If you have articles, blogs, or other content related to mathematics that you'd like to share, please contact our admin team for more information on how to contribute."
+/>
+        </div>
+        <div className="mx-auto">
+          <img src={FAQ} alt="FAQ" className="w-full pt-8" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FAQs;
