61a127bb27973da8d9e80033b2f9a99f5a2b2e31
TSOlami <tijani.saheed@yahoo.com>
2023-10-07 18:24:48 +0100

    add better responsive features

--- frontend/src/sections/ContactUs.jsx
+++ frontend/src/sections/ContactUs.jsx
@@ -3,7 +3,7 @@ import { contactUs } from "../assets"
 const ContactUs = () => {
   return (
 	<div>
-    <h1>Contact Us</h1>
+    <h1 className="header-text text-center max-w-xl mx-auto">Contact Us</h1>
     <div>
      <img src={contactUs} alt="" />
     </div>
--- frontend/src/sections/Features.jsx
+++ frontend/src/sections/Features.jsx
@@ -7,16 +7,15 @@ const Features = () => {
     <h1 className="header-text text-center max-w-xl mx-auto">Why You Should Use NAMSSN Website</h1>
     <div className="flex flex-col justify-center items-center pt-16 sm:pt-10">
       <div className="flex flex-col lg:flex-row gap-10">
-        <div className="relative flex-row-reverse bg-tertiary mx-auto rounded-2xl shadow-xl">
-         <div className="flex flex-col items-start px-10 py-20 ">
-         <h3 className="text-3xl sm:text-2xl font-bold font-crimson">Blog</h3>
-         <p className="text-xl font-crimson mx-auto">Get access to daily educational blog posts to keep you informed about the daily happenings in the department. Get your questions answered without issues and never be clueless about the department</p>
-         <img src={image4} alt="" />
+      <div className="flex flex-row bg-tertiary mx-auto rounded-2xl shadow-xl">
+         <div className="flex flex-col justify-center px-8 pt-20 pb-10">
+         <h3 className="text-3xl font-bold font-crimson">Blog</h3>
+         <p className="text-xl font-crimson">Get access to daily educational blog posts to keep you informed about the daily happenings in the department. Get your questions answered without issues and never be clueless about the department</p>
          </div>
-         
+         <img src={image4} alt="" />
         </div>
-        <div className="relative flex-row bg-tertiary rounded-2xl max-w-[25rem] mx-auto shadow-xl">
-         <div className="flex flex-col items-start px-10 py-20">
+        <div className="flex flex-row bg-tertiary rounded-2xl max-w-[25rem] mx-auto shadow-xl">
+         <div className="flex flex-col justify-center px-10 pt-20 pb-10">
          <h3 className="text-3xl font-bold font-crimson">Blog</h3>
          <p className="text-xl font-crimson mx-auto">Get access to daily educational blog posts to keep you informed about the daily happenings in the department. Get your questions answered without issues and never be clueless about the department</p>
          </div>
@@ -24,14 +23,14 @@ const Features = () => {
       </div>
       
       <div className="flex flex-col lg:flex-row gap-10 pt-8">
-        <div className="relative flex-row bg-tertiary rounded-2xl mx-auto max-w-[25rem] shadow-xl">
-         <div className="flex flex-col items-start px-10 py-20">
+        <div className="flex flex-row bg-tertiary rounded-2xl mx-auto max-w-[25rem] shadow-xl">
+         <div className="flex flex-col justify-center px-10 pt-20 pb-10">
          <h3 className="text-3xl font-bold font-crimson">Blog</h3>
          <p className="text-xl font-crimson">Get access to daily educational blog posts to keep you informed about the daily happenings in the department. Get your questions answered without issues and never be clueless about the department</p>
          </div>
         </div>
-        <div className="relative flex-row-reverse bg-zinc-900 mx-auto rounded-2xl shadow-xl">
-         <div className="flex flex-col items-start px-10 py-20">
+        <div className="flex flex-row bg-zinc-900 mx-auto rounded-2xl shadow-xl">
+         <div className="flex flex-col justify-center px-10 pt-20 pb-10">
          <h3 className="text-3xl text-white font-bold font-crimson">Blog</h3>
          <p className="text-xl text-white font-crimson">Get access to daily educational blog posts to keep you informed about the daily happenings in the department. Get your questions answered without issues and never be clueless about the department</p>
          </div>
