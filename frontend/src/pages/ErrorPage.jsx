627b7cc499e45bb37124939c0081b81adb33a0a1
TSOlami <tijani.saheed@yahoo.com>
2023-10-08 15:41:48 +0100

    add the footer

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
     <div className="w-full h-screen p-10 flex flex-col items-center">
       <div className="text-3xl font-bold text-center">Error 404</div>
       <div className="text-center text-lg">Sorry, we can&apos;t find what you&apos;re looking for</div>
-      <Link to='/' className="p-3 bg-primary rounded-xl text-white hover:opacity-75 m-10">Go to Homepage</Link>
+      <Link to='/' className="button-2 hover:opacity-75 m-10">Go to Homepage</Link>
     </div>
   )
 }
