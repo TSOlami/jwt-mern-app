fe7eecd157b8f2c158d97eb1b5e7f2cd277f6f97
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-08 19:14:22 +0100

    Little fix

--- frontend/src/sections/AboutUs.jsx
+++ frontend/src/sections/AboutUs.jsx
@@ -1,6 +1,6 @@
+import { Link } from 'react-router-dom';
 import aboutIcon from '../assets/icons/about.svg';
-import arrow from '../assets/icons/arrow.svg';
-
+import { BsArrowUpRight } from 'react-icons/bs';
 const AboutUs = () => {
   return (
     <section id="about-us" className="flex lg:flex-row flex-col justify-center gap-8 max-container">
@@ -12,13 +12,13 @@ const AboutUs = () => {
           height={70}
         /> <h1 className='header-text'>About Us</h1>
       </div>
-      <div className='flex-col'>
+      <div className='flex flex-col gap-5'>
         <p className="text-xl font-crimson">
           At NAMSSN, we foster a community of passionate individuals who share a common love for mathematics and its applications. Our association brings together students, faculty, and enthusiasts to explore, learn, and grow together. Mathematics Department is hard like mad and i dont see wy a person would want a website designed for it. But i would try to make it beautiful sha. Using Illustratons and images
         </p>
-        <div className="text-container flex border-primary">
-        Know more about us<img src={arrow} alt="arrow" />
-        </div>
+        <Link to='/about-us' className="text-container flex border-primary w-fit">
+        Know more about us <BsArrowUpRight color='#17A1FA'/>
+        </Link>
       </div>
       
     </section>
