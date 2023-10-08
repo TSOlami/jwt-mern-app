1be08bd1d990d5ec8c82fc90e4550ed226119587
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-10-08 13:13:38 +0100

    Fully functional + responsive styling overhaul

--- frontend/src/components/nav/NavBar.jsx
+++ frontend/src/components/nav/NavBar.jsx
@@ -1,44 +1,80 @@
-import { Hamburger, NamssnLogo } from "../../assets"
-import { navLinks } from "../../constants"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { NamssnLogo } from "../../assets";
+import { navLinks } from "../../constants";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return (
-	<header className="padding-x py-[8px] absolute z-[10px] w-full">
-    <nav className="flex justify-between items-center max-container">
-      <div className="logo-container">
-        <a href="/">
-          <img src={NamssnLogo} alt="Logo" className="logo" />
-        </a>
-        <span className="logo-text py-[2px]">NAMSSN</span>
-      </div>
-      <ul className="flex-1 flex justify-center items-center ml-8 gap-10 max-lg:hidden">
-        {navLinks.map((item) => (
-          <li key={item.label}>
-            <Link
-            to={item.href}
-            className="nav-text"
-            >
-            {item.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className="hidden max-lg:block">
-        <img
-          src={Hamburger}
-          alt="Menu"
-          width={25}
-          height={25}
-        />
-      </div>
-      <div className=" flex gap-4">
-        <button className="button-1">Sign Up</button>
-        <button className="button-2">Log In</button>
-      </div>
-    </nav>
-  </header>
-  )
-}
+	const [isNavOpen, setIsNavOpen] = useState(false);
+	const handleNavOpen = () => {
+		setIsNavOpen(!isNavOpen);
+	};
 
-export default NavBar
+	useEffect(() => {
+		const hamburger = document.querySelector(".hamburger");
+
+		hamburger.addEventListener("click", handleNavOpen);
+
+		return () => {
+			hamburger.removeEventListener("click", handleNavOpen);
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isNavOpen]);
+
+	return (
+		<header className="px-[10px] py-[8px] absolute z-[10px] w-full">
+
+
+			{/* Namssn logo */}
+			<div>
+				<div className="flex flex-row items-center absolute lg:hidden">
+					<Link to="/">
+						<img src={NamssnLogo} alt="Logo" className="logo" />
+					</Link>
+					<span className="logo-text py-[2px]">NAMSSN</span>
+				</div>
+			</div>
+
+			{/* Navbar */}
+			<nav
+				className={
+					isNavOpen
+						? "bg-white flex flex-col lg:flex-row lg:justify-between md:items-center h-screen lg:h-auto z-10 absolute left-0 w-[315px] lg:static lg:w-auto transition-all duration-400"
+						: "bg-white flex flex-col lg:flex-row lg:justify-between md:items-center h-screen lg:h-auto z-10 absolute left-[-100%] w-[315px] lg:static lg:w-auto transition-all duration-400"
+				}
+			>
+				<div className="logo-container">
+					<Link to="/">
+						<img src={NamssnLogo} alt="Logo" className="logo" />
+					</Link>
+					<span className="logo-text py-[2px]">NAMSSN</span>
+				</div>
+				<ul className="flex-1 flex justify-center lg:justify-evenly items-center ml-6 gap-10 lg:gap-3 flex-col lg:flex-row">
+					{navLinks.map((item) => (
+						<li key={item.label}>
+							<Link to={item.href} className="nav-text">
+								{item.label}
+							</Link>
+						</li>
+					))}
+				</ul>
+
+				<div className="pb-14 lg:pl-5 lg:p-0 gap-4 flex mx-4 items-center justify-center">
+					<Link to="/signup" className="button-1">
+						Sign Up
+					</Link>
+					<Link to="/signin" className="button-2">
+						Log In
+					</Link>
+				</div>
+			</nav>
+
+			<div className={isNavOpen ? "hamburger active" : "hamburger"}>
+				<span className="bar side"></span>
+				<span className="bar side"></span>
+				<span className="bar side"></span>
+			</div>
+		</header>
+	);
+};
+
+export default NavBar;
